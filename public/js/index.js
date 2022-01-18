d0783dfbbd606f576041df949ed6c2b0ca96e53a
tiara <niceefforts42@gmail.com>
2022-01-18 11:54:57 +0700

    tugas1

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,35 @@
  *  ====================================
  *  Code App kita
  *  Code di bawah ini tidak perlu diubah
- * 
+ *
  */
-$(document).ready(function() {
+$(document).ready(function () {
   App.getInstance();
-  
+
   const options = {
-    button: '#update-sw',
-    toast: '.notification-update',
+    button: "#update-sw",
+    toast: ".notification-update",
   };
 
   new SW(options);
 });
+
+function changeColor(color) {
+  document.querySelector(".nav-wrapper.custom-theme").setAttribute("style", `background-color: ${color} !important;`);
+}
+
+// function menu1() {
+//   changeColor("green");
+// }
+// function menu2() {
+//   changeColor("yellow");
+// }
+
+// $("p").on("click", function () {
+//   $("nav").css("background", "#ccc");
+// });
+
+// let a = document.getElementById('menu1')
+// a.addEventListener('click', function() {
+//   a.style.backgroundColor = 'green'
+// })
