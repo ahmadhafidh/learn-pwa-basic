668b69b5abd2428e52ac8de82f3404afb18658fd
widhianiagustinn <widhianiagustin14@gmail.com>
2022-01-18 12:05:21 +0700

    tugas pertemuan 4

--- public/js/app.js
+++ public/js/app.js
@@ -1,66 +1,62 @@
-const App = (function() {
+const App = (function () {
   let instance;
 
   function timelineTemplate(data) {
-    const tmpl = '<li class="collection-item">'
-    +   '<p class="title">' + data.title + '</p>'
-    +   '<p class="author">Author: ' + data.userId + '</p>'
-    +   '<div class="body">' + data.body + '</div>'
-    +   '</li>';
+    const tmpl = '<li class="collection-item">' + '<p class="title">' + data.title + "</p>" + '<p class="author">Author: ' + data.userId + "</p>" + '<div class="body">' + data.body + "</div>" + "</li>";
 
     return tmpl;
   }
 
   function timeline() {
-    const timelineDOM = $('#timeline');
-    const preloader = $('#preloader');
-    let list = '';
+    const timelineDOM = $("#timeline");
+    const preloader = $("#preloader");
+    let list = "";
 
     $.ajax({
-      method: 'GET',
-      url: 'https://jsonplaceholder.typicode.com/posts',
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/posts",
     })
-      .done(function(data) {
+      .done(function (data) {
         if (!data) {
-          preloader
-            .hide()
-            .find('> div')
-            .removeClass('active');
-          timelineDOM.append('<h3>Error while fetching the data!</h3>');
+          preloader.hide().find("> div").removeClass("active");
+          timelineDOM.append("<h3>Error while fetching the data!</h3>");
           return;
         }
 
-        data.map(function(d) {
+        data.map(function (d) {
           list += timelineTemplate(d);
           return d;
         });
 
-        timelineDOM.html('<ul class="collection">' + list + '</ul>');
+        timelineDOM.html('<ul class="collection">' + list + "</ul>");
       })
-      .error(function(err) {
-        preloader
-          .hide()
-          .find('> div')
-          .removeClass('active');
+      .error(function (err) {
+        preloader.hide().find("> div").removeClass("active");
         timelineDOM.append('<h3 class="error">Error while fetching the data!</h3>');
-        console.error(err)
-      })
+        console.error(err);
+      });
   }
 
   function init() {
     timeline();
 
-    $('.button-collapse').sideNav();
-    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $(".button-collapse").sideNav();
+    $(".carousel.carousel-slider").carousel({ fullWidth: true });
   }
 
   return {
-    getInstance: function() {
+    getInstance: function () {
       if (!instance) {
         instance = init();
       }
 
       return instance;
-    }
-  }
+    },
+  };
 })();
+
+function navColor(color) {
+  document
+    .querySelector(".custom-theme")
+    .setAttribute("style", `background-color: ${color} !important;`);
+}
