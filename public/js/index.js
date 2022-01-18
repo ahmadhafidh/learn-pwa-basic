81d56141804df1bfe9f903c0367891e428afaf67
Agung Hidayat <agunghidayat10@gmail.com>
2022-01-18 10:06:34 +0700

    Tugas #4 Agung Hidayat

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,21 @@
  *  ====================================
  *  Code App kita
  *  Code di bawah ini tidak perlu diubah
- * 
+ *
  */
-$(document).ready(function() {
+$(document).ready(function () {
   App.getInstance();
-  
+
   const options = {
-    button: '#update-sw',
-    toast: '.notification-update',
+    button: "#update-sw",
+    toast: ".notification-update",
   };
 
   new SW(options);
 });
+
+function changeColorBG(color) {
+  document
+    .querySelector(".nav-wrapper.custom-theme")
+    .setAttribute("style", `background-color: ${color} !important;`);
+}
